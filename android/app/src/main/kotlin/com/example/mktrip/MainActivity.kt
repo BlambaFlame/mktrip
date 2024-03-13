ec497934ae3fcca13c69f8799e6757c28a203739
prcstak <g.marsel2002@yandex.ru>
2024-03-13 13:09:58 +0300

    ребятушки работаем

--- android/app/src/main/kotlin/com/example/mktrip/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mktrip/MainActivity.kt
@@ -1,3 +1,4 @@
+package com.example.mktrip
 
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:mktrip/map/domain/app_lat_long.dart';
+import 'package:mktrip/map/domain/location_service.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 class MapPage extends StatefulWidget {
@@ -14,12 +16,59 @@ class _MapPageState extends State<MapPage> {
   final mapControllerCompleter = Completer<YandexMapController>();
 
   @override
+  void initState() {
+    super.initState();
+    _initPermission().ignore();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Текущее местоположение'),
+      ),
       body: YandexMap(
         onMapCreated: (controller) {
           mapControllerCompleter.complete(controller);
         },
+      ),
+    );
+  }
+
+  /// Проверка разрешений на доступ к геопозиции пользователя
+  Future<void> _initPermission() async {
+    if (!await LocationService().checkPermission()) {
+      await LocationService().requestPermission();
+    }
+    await _fetchCurrentLocation();
+  }
+
+  /// Получение текущей геопозиции пользователя
+  Future<void> _fetchCurrentLocation() async {
+    AppLatLong location;
+    const defLocation = MoscowLocation();
+    try {
+      location = await LocationService().getCurrentLocation();
+    } catch (_) {
+      location = defLocation;
+    }
+    _moveToCurrentLocation(location);
+  }
+
+  /// Метод для показа текущей позиции
+  Future<void> _moveToCurrentLocation(
+      AppLatLong appLatLong,
+      ) async {
+    (await mapControllerCompleter.future).moveCamera(
+      animation: const MapAnimation(type: MapAnimationType.linear, duration: 1),
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: Point(
+            latitude: appLatLong.lat,
+            longitude: appLatLong.long,
+          ),
+          zoom: 12,
+        ),
       ),
     );
   }
